9faff7554862d3b0aa52d4be0f4e56906896e756
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-02-18 11:56:37 -0300

    fix: Layoult

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,9 @@ import GlobalClickEvent from "./context/GlobalClickEvent.jsx";
 
 // Altere aqui para "LP" ou "site"
 const mode = "LP"; // só muda isso e o resto se adapta
+requestAnimationFrame(() => {
+  root.classList.add("ready");
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ColorModeProvider>
